32d92306bc0d8c48b40d3219d84e36e10d6e5f36
tnals123 <soo8608@naver.com>
2021-09-23 15:47:34 +0900

    update my practice

---
+++ youtubelogic.js
@@ -0,0 +1,18 @@
+function ChangeColor(changepoint){
+    var changesection=document.getElementById(changepoint)
+    changesection.style.background="#C0C0C0"
+}
+function ChangeColor_Click(changepoint){
+    var changesection=document.getElementById(changepoint)
+    changesection.style.background="#808080"
+}
+
+function ChangeColor_AfterClick(changepoint){
+    var changesection=document.getElementById(changepoint)
+    changesection.style.background="#C0C0C0"
+}
+
+function ChangeColor_Origin(changepoint){
+    var changesection=document.getElementById(changepoint)
+    changesection.style.background="white"
+}
