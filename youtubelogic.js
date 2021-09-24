13e73ed3c288b8a1e4a9319e2c03909518f2bb43
tnals123 <soo8608@naver.com>
2021-09-25 02:18:57 +0900

    update my practice

--- youtubelogic.js
+++ youtubelogic.js
@@ -1,3 +1,5 @@
+var hidden_menu_clicked=0;
+
 function ChangeColor(changepoint){
     var changesection=document.getElementById(changepoint)
     changesection.style.background="#C0C0C0"
@@ -17,8 +19,33 @@ function ChangeColor_Origin(changepoint){
     changesection.style.background="white"
 }
 
-function Appear_Hidden_Menu(){
-    var slidescreen=document.getElementById("asdf");
-    console.log(slidescreen);
-    console.log("asdf");
+function Open_Hidden_Menu(){
+    var asdf=document.getElementById("asdf");
+    var videoscreen=document.getElementById("background");
+    videoscreen.style.display="block";
+    videoscreen.style.transition="1s";
+    console.log(hidden_menu_clicked);
+    asdf.style.left=0.1+'px';
+    asdf.style.transition= "0.3s";
+    hidden_menu_clicked=1;
+}
+
+function Close_Hidden_Menu(){
+    var asdf=document.getElementById("asdf");
+    console.log(hidden_menu_clicked);
+    var videoscreen=document.getElementById("background");
+    videoscreen.style.display="none";
+    videoscreen.style.transition="1s";
+    asdf.style.left=-200+'px';
+    asdf.style.transition= "0.3s";
+    hidden_menu_clicked=0;
+}
+
+function asdf(){
+    if (hidden_menu_clicked==0){
+        Open_Hidden_Menu();
+    }
+    else if (hidden_menu_clicked==1){
+        Close_Hidden_Menu();
+    }
 }
